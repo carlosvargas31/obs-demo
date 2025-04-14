264d8df5812d9ae1b6c6c3884958841991215c14
Carlos Vargas <carlos.vargas@eccocar.com>
2025-04-14 12:24:06 -0500

    Adding new endpoints user crud, get users, update users, delete users

--- src/main/java/com/obs/obsdemo/application/UserService.java
+++ src/main/java/com/obs/obsdemo/application/UserService.java
@@ -1,9 +1,12 @@
 package com.obs.obsdemo.application;
 
+import com.obs.obsdemo.domain.NoContentException;
 import com.obs.obsdemo.domain.NotFoundException;
 import com.obs.obsdemo.domain.User;
 import com.obs.obsdemo.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +23,29 @@ public class UserService {
 
     public User getUserById(String id) {
         return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
+    public ResponseEntity<User> updateUser(String id, UserRequest user) {
+        return userRepository.findById(id)
+                .map(u -> {
+                    u.setFirstName(user.firstName());
+                    u.setLastName(user.lastName());
+                    u.setMarriedStatus(user.marriedStatus());
+
+                    return ResponseEntity.ok(userRepository.save(u));
+                })
+                .orElseGet(() -> createUser(user));
+    }
+
+    public void deleteUserById(String id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new NoContentException("User not found"));
+        userRepository.delete(user);
+    }
+
+    private ResponseEntity<User> createUser(UserRequest user) {
+        User newUser = UserMapper.mapUserRequestToUser(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRepository.insert(newUser));
     }
 
 }
