d16c8b58fc185a2b6777f1625f8a0735ff3ac6b0
Carlos Vargas <carlos.vargas@eccocar.com>
2025-04-14 11:13:39 -0500

    Initial springboot project

---
+++ src/main/java/com/obs/obsdemo/ObsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.obs.obsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/obs/obsdemo/ObsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obs.obsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
